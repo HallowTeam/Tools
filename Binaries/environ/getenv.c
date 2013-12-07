a37f029f2781dca5719e712dd1496b47167becb9
RageYL <vic1708@gmail.com>
2013-12-07 22:39:31 +0100

    environ

--- Binaries/environ/getenv.c
+++ Binaries/environ/getenv.c
@@ -7,11 +7,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int     exists(char *name)
-{
-	return open(name, O_RDONLY);
-}
-
 int     main(int argc, char **argv)
 {
 	int   offset;
@@ -29,23 +24,31 @@ int     main(int argc, char **argv)
 		return 0;
 	}
 
-	if (exists(argv[2]) == -1)
+	if (open(argv[2], O_RDONLY) == -1)
 	{
 		printf("%s doesn't exist or is not readable\n", argv[2]);
 		return 0;
 	}
 
-	offset  = 0;
-	offset  = strlen(realpath(argv[0], NULL)) - strlen(realpath(argv[2], NULL));
-	offset *= 2;
-	addr   += offset;
+	offset = strlen(realpath(argv[0], NULL)) - strlen(realpath(argv[2], NULL));
 
-	printf("Address: \\x%02lx\\x%02lx\\x%02lx\\x%02lx (%08lx)",
+	printf("Path argv[0]    : %s\n", realpath(argv[0], NULL));
+	printf("Path argv[2]    : %s\n", realpath(argv[2], NULL));
+	printf("Offset          : %d\n", offset);
+	printf("Address argv[0] : \\x%02lx\\x%02lx\\x%02lx\\x%02lx (%#08lx)\n",
 				 (0x000000FF & (long)addr) >> 0,
 				 (0x0000FF00 & (long)addr) >> 8,
 				 (0x00FF0000 & (long)addr) >> 16,
-				 (0xFF000000 & (long)addr) >> 24,
-				 (long)addr);
+				 (0xFF000000 & (long)addr) >> 24, (long)addr);
+	printf("Address argv[2] : \\x%02lx\\x%02lx\\x%02lx\\x%02lx (%#08lx)\n",
+				 (0x000000FF & (long)addr + offset) >> 0,
+				 (0x0000FF00 & (long)addr + offset) >> 8,
+				 (0x00FF0000 & (long)addr + offset) >> 16,
+				 (0xFF000000 & (long)addr + offset) >> 24, (long)addr + offset);
 
 	return 0;
 }
+
+/*******************************************************************************
+	env -i SHLVL=0 PWD=$PWD $PWD/PROG
+ ******************************************************************************/
