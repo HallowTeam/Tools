4e5ae0743cb15c98b4fb0548678ac325f1f34e92
RageYL <vic1708@gmail.com>
2013-12-08 17:25:08 +0100

    xor

--- Crypto/xor.py
+++ Crypto/xor.py
@@ -2,8 +2,10 @@
 # coding: utf-8
 
 import string
+from termcolor import colored
 
-charset = set(string.printable) - set(string.whitespace) | set(" ")
+KEY_CHARSET = set(string.printable) - set(string.whitespace) - set("!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~")
+TXT_CHARSET = set(string.printable) - set("\"#$%&*+/<=>@[\\]^`|~")
 
 def split_list(list_, chunksize = 2):
     return [list_[i:i + chunksize] for i in xrange(0, len(list_), chunksize)]
@@ -15,24 +17,45 @@ class Xor():
             cipher += chr(ord(c) ^ ord(key[i % len(key)]))
         return cipher
 
-    def force(self, cipher, key_len, key_charset = charset, cipher_charset = charset, callback = None):
+    def force(self, cipher, key_len, key_charset = KEY_CHARSET, txt_charset = TXT_CHARSET, callback = None):
         keys   = []
+        chars  = set()
         chunks = split_list(cipher, key_len)
         for i in xrange(key_len):
+            ignored = set()
             for c in key_charset:
+                charset = set()
                 success = True
                 for chunk in chunks:
-                    if i < len(chunk) and not self.xor(chunk[i], c) in cipher_charset:
-                        success = False
-                        break
+                    if i < len(chunk):
+                        char = self.xor(chunk[i], c)
+                        if not char in txt_charset:
+                            ignored |= set(char)
+                            success  = False
+                            break
+                        charset |= set(char)
+                    else:
+                        continue
                 if success:
                     if callback and not callback(i, c, keys):
                         continue
                     if i == len(keys):
                         keys.append([])
                     keys[i].append(c)
+                    chars |= charset
             if len(keys) <= i:
-                print "[-] Nothing found at %d" % i
-                print "[-] " + str(keys)
-                break
-        return keys
+                return False, keys, sorted(chars), sorted(ignored)
+        return True, keys, sorted(chars), None
+
+    def format(self, success, keys, chars, ignored):
+        if success == True:
+            print colored("[+] Success", "green")
+            print "[+] Charset:", chars
+            print colored("[+] Keys   :", "green"), colored(keys, "yellow")
+        else:
+            print colored("[-] Failure ", "red"), "at index %d" % len(keys)
+            print "[-] Charset:", chars
+            print "[-] Keys   :", keys
+            print colored("[-] Ignored:", "red"), colored(ignored, "yellow")
+        return success, keys, chars, ignored
+
